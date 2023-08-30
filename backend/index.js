73fcdd9e0e456693abd4f3cd98239ef7dc4c9ac7
Fjean <fjean0522@gmail.com>
2023-08-29 20:47:25 -0400

    Create routes & fill in with test data.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const workoutRouts = require('./routes/workouts');
 
+// Middleware
+app.use(express.json());
 
+// Routes
+app.use('/api/workouts', workoutRouts);
 
-
+// Listen for requests
 app.listen(port, () => {
     console.log('Listening on port ' + port);
 });
+
 
