7b48fea135b38f1a1c0d04299a98152aed74119d
Fjean <fjean0522@gmail.com>
2023-08-30 11:34:16 -0400

    Connect to database.
    Listen for request upon conection.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 const workoutRouts = require('./routes/workouts');
@@ -10,9 +11,12 @@ app.use(express.json());
 // Routes
 app.use('/api/workouts', workoutRouts);
 
-// Listen for requests
-app.listen(port, () => {
-    console.log('Listening on port ' + port);
-});
-
-
+// Connect to db
+mongoose.connect(process.env.DB_URI)
+    .then(() => {
+        // Listen for requests
+        app.listen(port, () => {
+        console.log('Listening on port ' + port)
+        });
+    })
+    .catch((err) => console.log(err));
