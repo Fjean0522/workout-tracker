46090d2d2a321cc23802599c2ea023629f9bee64
Fjean <fjean0522@gmail.com>
2023-09-01 20:00:03 -0400

    Use react context to update page.
    Automatically update page after new workout is successfully added.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+
+// Components
 import WorkoutDetails from '../components/WorkoutDetails';
 import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null)
+  const {workouts, dispatch} = useWorkoutsContext()
 
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch('/api/workouts')
       const json = await response.json()
 
-      response.ok? setWorkouts(json) : null
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      };
     };
 
     fetchWorkouts()
